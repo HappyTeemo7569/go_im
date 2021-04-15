b70d09474330fdc1d7b6c8d0acd41e13879bf59a
HappyTeemo7569 <1726214027@qq.com>
2021-04-15 14:12:42 +0800

    使用kit库

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_im
+
+go 1.16
+
+require github.com/HappyTeemo7569/teemoKit v1.0.2-0.20210415032632-ee8ed36a1110
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/HappyTeemo7569/teemoKit/tlog"
+)
+
+func main() {
+	tlog.Info("11")
+}
